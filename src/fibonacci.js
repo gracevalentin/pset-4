bce3aab75598bc5b3c595cc8a40ade17facfc03b
Grace Valentin <grace.valentin@me.com>
2019-11-20 13:21:45 -0500

    grace is stupid

--- src/fibonacci.js
+++ src/fibonacci.js
@@ -1,12 +1,12 @@
 const readlineSync = require("readline-sync");
 
-const MIN = 0;
-const MAX = Number.MAX_SAFE_INTEGER;
-let NonNegativeInteger = -1
+const MIN = 1;
+const MAX = 78;
+
 console.log ()
 do {
-  let NonNegativeInteger = Number(readlineSync.question("Non-negative integer: "));
-} while (NonNegativeInteger > MIN || NonNegativeInteger < MAX);
+  let PositiveInteger = Number(readlineSync.question("Positive integer: "));
+} while (PositiveInteger > MIN || PositiveInteger < MAX);
 
 if (NonNegativeInteger % 1==0 || NonNegativeInteger % NonNegativeInteger==0){
   console.log("\nPrime.\n")
